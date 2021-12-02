c133290a90fb801e5d58d545d0fe8875b279a2da
ws1g17 <ws1g17@soton.ac.uk>
2021-12-02 15:30:18 +0000

    day1 done

---
+++ day1.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    fun readFileAsLinesUsingUseLines(fileName: String): List<Int>
+            = File(fileName).useLines { it.toList() }.map{it.toInt()}
+
+    val lines = readFileAsLinesUsingUseLines("src\\day1\\input")
+
+    var current = lines[0]
+    var total = 0
+    for(i in lines.indices){
+        if (lines[i] > current){
+            total += 1
+        }
+        current = lines[i]
+    }
+
+    print("A: $total")
+}
